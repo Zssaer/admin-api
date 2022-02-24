3c2b394c44fe170102ef931e40fa86ea1666d4c4
Zssaer <ntc001@163.com>
2022-02-24 11:10:55 +0800

    新增Screw生成数据库设计文档

--- README.md
+++ README.md
@@ -9,12 +9,13 @@
 2. 内置MVC代码生成器。
 
 3. 你只需写上简单的表注释，即可直接生成：`java代码`、`后台管理接口`、`接口文档`等，使项目中80%的代码做到自动化，省时省力。
+
 4. 实现接口限流，轻松解决中小型项目下的接口QPS限流问题。
 
 
 ##  ⚡功能架构
 
-- 项目基于`springboot`搭建，以`freemarker`作为代码生成器模板
+- 项目基于`springboot 2.4.4`搭建，以`freemarker`作为代码生成器模板
 - 采用了如下第三方包：
   - Sa-Token
   - Redis
@@ -30,4 +31,5 @@
   - oss
   - excel-spring-boot-starter
   - quartz
+  - screw
 
---
+++ admin-provider/src/test/java/com/admin/provider/ScrewGenerator.java
@@ -0,0 +1,88 @@
+package com.admin.provider;
+
+import cn.smallbun.screw.core.Configuration;
+import cn.smallbun.screw.core.engine.EngineConfig;
+import cn.smallbun.screw.core.engine.EngineFileType;
+import cn.smallbun.screw.core.engine.EngineTemplateType;
+import cn.smallbun.screw.core.execute.DocumentationExecute;
+import cn.smallbun.screw.core.process.ProcessConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @description: 数据库结构文档生成器
+ * @author: Zhaotianyi
+ * @time: 2022/2/24 10:12
+ */
+public class ScrewGenerator {
+
+    public static void main(String[] args) {
+        ScrewGenerator generator = new ScrewGenerator();
+        generator.run();
+    }
+
+    public void run() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/admin-api");
+        hikariConfig.setUsername("root");
+        hikariConfig.setPassword("200428");
+        hikariConfig.addDataSourceProperty("useInformationSchema", "true");
+        DataSource dataSource = new HikariDataSource(hikariConfig);
+
+        // 生成文件配置
+        EngineConfig engineConfig = EngineConfig.builder()
+                // 生成文件路径，自己mac本地的地址，这里需要自己更换下路径
+                .fileOutputDir("DirFile/")
+                // 打开目录
+                .openOutputDir(false)
+                // 文件类型
+                .fileType(EngineFileType.HTML)
+                // 生成模板实现
+                .produceType(EngineTemplateType.freemarker).build();
+
+        // 生成文档配置（包含以下自定义版本号、描述等配置连接）
+        Configuration config = Configuration.builder()
+                .version("V1.0.0")
+                .description("")
+                .dataSource(dataSource)
+                .engineConfig(engineConfig)
+                .produceConfig(getProcessConfig())
+                .build();
+
+        // 执行生成
+        new DocumentationExecute(config).execute();
+    }
+
+    /**
+     * 配置想要生成的表+ 配置想要忽略的表
+     * @return 生成表配置
+     */
+    public static ProcessConfig getProcessConfig(){
+        // 忽略表名
+        List<String> ignoreTableName = Arrays.asList();
+        // 忽略表前缀，如忽略a开头的数据库表
+        List<String> ignorePrefix = Arrays.asList();
+        // 忽略表后缀
+        List<String> ignoreSuffix = Arrays.asList();
+
+        return ProcessConfig.builder()
+                //根据名称指定表生成
+                .designatedTableName(new ArrayList<>())
+                //根据表前缀生成
+                .designatedTablePrefix(new ArrayList<>())
+                //根据表后缀生成
+                .designatedTableSuffix(new ArrayList<>())
+                //忽略表名
+                .ignoreTableName(ignoreTableName)
+                //忽略表前缀
+                .ignoreTablePrefix(ignorePrefix)
+                //忽略表后缀
+                .ignoreTableSuffix(ignoreSuffix).build();
+    }
+}
